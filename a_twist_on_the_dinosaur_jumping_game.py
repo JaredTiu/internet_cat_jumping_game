0daf8f22ca345b52a1a478695a82621247f81528
unknown <jaredtiu1706@gmail.com>
2024-10-02 01:01:22 +0800

    added images and a method to safely exit the window of the game

--- a_twist_on_the_dinosaur_jumping_game.py
+++ a_twist_on_the_dinosaur_jumping_game.py
@@ -7,3 +7,15 @@ screen_height = 600
 screen_width = 1100
 screen = pygame.display.set_mode((screen_width, screen_height))
 
+running = [pygame.image.load(os.path.join("Assets/64x64", "run1.png")),
+        pygame.image.load(os.path.join("Assets/64x64", "run2.png"))]
+Jumping = pygame.image.load(os.path.join("Assets/64x64", "jump1.png"))
+
+
+def main () : 
+    run = True 
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
